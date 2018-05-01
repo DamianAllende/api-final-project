1bfebb89456fb71b5488c3d4f4f1550500831037
DamianAllende <amigosaburridos@gmail.com>
2018-05-01 15:18:07 -0500

    Crear servidor localhost  en  [server.js]

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', function(req, res){
+    res.send('home page')
+})
+
+
+
+const PORT = process.env.PORT || 3000;app.listen(PORT, function(){
+    console.log(`App running in PORT: ${PORT}`)
+})
